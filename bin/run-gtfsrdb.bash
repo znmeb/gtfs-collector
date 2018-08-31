1b0eee72de9df8024331c182afd143611fbfadff
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-31 03:41:42 -0700

    fix crashes

--- bin/run-gtfsrdb.bash
+++ bin/run-gtfsrdb.bash
@@ -3,12 +3,11 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-workon gtfsdb
-pushd ~/Projects/gtfsrdb
-python ./gtfsrdb.py \
-  -t ${TRIP_UPDATE_URL}?appID=${appID} \
-  -a ${SERVICE_ALERT_URL}?appID=${appID} \
-  -p ${VEHICLE_POSITION_URL}?appID=${appID} \
-  -d ${DATABASE_URL} -c
-popd
+cd ~/Projects/gtfsrdb
+python3 ./gtfsrdb.py \
+  --trip-updates ${TRIP_UPDATE_URL}?appID=${appID} \
+  --alerts ${SERVICE_ALERT_URL}?appID=${appID} \
+  --vehicle-positions ${VEHICLE_POSITION_URL}?appID=${appID} \
+  --database ${DATABASE_URL} \
+  --create-tables \
+  --quiet
