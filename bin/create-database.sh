af2a524c4aed17d0b21e7c7ee1e9861bfb71d26b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-30 21:08:34 -0700

    no superuser for gtfsdb role

--- bin/create-database.sh
+++ bin/create-database.sh
@@ -3,6 +3,6 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
-createuser -s gtfsdb
+createuser gtfsdb
 createdb -O gtfsdb gtfsdb
 psql -U gtfsdb -d gtfsdb -c "CREATE EXTENSION postgis CASCADE;"
