8cc9cd3f3c04ff1c7966a6125a3a92d337d99416
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-30 21:19:50 -0700

    Revert "no superuser for gtfsdb role"
    This reverts commit af2a524c4aed17d0b21e7c7ee1e9861bfb71d26b.

--- bin/create-database.sh
+++ bin/create-database.sh
@@ -3,6 +3,6 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
-createuser gtfsdb
+createuser -s gtfsdb
 createdb -O gtfsdb gtfsdb
 psql -U gtfsdb -d gtfsdb -c "CREATE EXTENSION postgis CASCADE;"
