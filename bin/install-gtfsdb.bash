96f4df34466bb854f2a085896a2f86f72adc0bc1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-31 03:39:23 -0700

    cleanup

--- bin/install-gtfsdb.bash
+++ bin/install-gtfsdb.bash
@@ -3,33 +3,16 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
+echo "Cloning the repositories"
+mkdir -p ~/Projects; cd ~/Projects
+git clone https://github.com/OpenTransitTools/gtfsdb.git
+git clone https://github.com/znmeb/gtfsrdb.git
+
 echo "Creating 'gtfsdb' virtual environment"
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 mkvirtualenv gtfsdb --python=/usr/bin/python3
-pip install --upgrade zc.buildout google sqlalchemy
-
-echo "Cloning the repositories if needed"
-mkdir -p ~/Projects; pushd ~/Projects
-
-if [ ! -d gtfsdb ]
-then
-  git clone https://github.com/OpenTransitTools/gtfsdb.git
-fi
-
-if [ ! -d gtfsrdb ]
-then
-  git clone https://github.com/znmeb/gtfsrdb.git
-fi
+pip install --upgrade zc.buildout sqlalchemy
 
 echo "Installing gtfsdb"
 cd gtfsdb
-git pull
 buildout install prod postgresql
-cd ..
-
-echo "Updating gtfsrdb"
-cd gtfsrdb
-git pull
-cd ..
-
-popd
