fecb233a24af2a11541195302b5bae6993d557e6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-31 23:38:39 -0700

    let gtfsdb user log in to PostgreSQL

--- postgis-scripts/create-database.sh
+++ postgis-scripts/create-database.sh
@@ -4,17 +4,10 @@
 # License: MIT
 
 # create an unprivileged user for the database
-createuser \
-  --no-createdb \
-  --no-inherit \
-  --login \
-  --no-createrole \
-  --no-superuser \
-  --no-replication \
-  gtfsdb
+createuser gtfsdb
 command="ALTER USER gtfsdb WITH PASSWORD '${GTFSDB_PASSWORD}';"
 psql --username=postgres -c "$command"
 
 # create the database
 createdb --owner=gtfsdb gtfsdb
-psql --username=gtfsdb --dbname=gtfsdb -c "CREATE EXTENSION postgis CASCADE;"
+psql --username=postgres --dbname=gtfsdb -c "CREATE EXTENSION postgis CASCADE;"
