2a848864946f0c6102c14a7d6850dae57d3c38e8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-09-01 12:12:46 -0700

    Not needed any more

--- host-scripts/login.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [ "$#" -gt 0 ]
-then
-  docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u ${1} gtfs-collector_gtfs-collector_1 /bin/bash
-else
-  docker exec -it -e LINES=$(tput lines) -e COLUMNS=$(tput cols) -u gtfsdb gtfs-collector_gtfs-collector_1 /bin/bash
-fi
