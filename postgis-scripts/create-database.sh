6123a1b4e03cb9ba9974197e665b277fe3baf0bf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-08-31 21:44:23 -0700

    add the non-privileged user

--- postgis-scripts/create-database.sh
+++ postgis-scripts/create-database.sh
@@ -3,6 +3,17 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
-createuser -s gtfsdb
-createdb -O gtfsdb gtfsdb
-psql -U gtfsdb -d gtfsdb -c "CREATE EXTENSION postgis CASCADE;"
+# create an unprivileged user for the database
+createuser \
+  --no-createdb \
+  --no-inherit \
+  --no-login \
+  --no-createrole \
+  --no-superuser \
+  --no-replication \
+  gtfsdb
+command="ALTER USER gtfsdb WITH PASSWORD '${GTFSDB_PASSWORD}';"
+psql --username=postgres -c "$command"
+
+# create the database
+createdb --owner=gtfsdb gtfsdb
