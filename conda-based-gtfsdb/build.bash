5f5c0e49ddb2456b1952168c2c7ba59940be959f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-09-03 17:36:09 -0700

    installer for Git submodules using Conda

---
+++ conda-based-gtfsdb/build.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+#
+# Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
+# License: MIT
+
+echo "Creating 'gtfsdb' virtual environment"
+if [ `conda env list|grep gtfsdb|wc -l` -gt "0" ]
+then 
+  echo "Removing old environment"
+  conda env remove -n gtfsdb -y
+fi
+
+conda create -y -n gtfsdb python=3
+source activate gtfsdb
+pip install --upgrade pip
+pip install --upgrade zc.buildout sqlalchemy psycopg2-binary
+
+echo "Building the binary"
+cd gtfsdb
+git fetch
+git merge
+buildout install prod postgresql
