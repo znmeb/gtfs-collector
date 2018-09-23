c0cfa680c116aba37020d87b313aefac2c75aa7a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2018-09-22 22:26:16 -0700

    auto-start collector!

--- gtfsrdb-scripts/run-gtfsrdb.bash
+++ gtfsrdb-scripts/run-gtfsrdb.bash
@@ -3,6 +3,15 @@
 # Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
 # License: MIT
 
+export PGPASSWORD=$POSTGRES_PASSWORD
+until psql -h postgis -U postgres -p 5432 -d postgres -c '\q'
+do
+  >&2 echo "postgis service is unavailable - sleeping"
+  sleep 15
+done
+
+>&2 echo "postgis service is up"
+
 cd /usr/local/src/gtfsrdb
 python ./gtfsrdb.py \
   --trip-updates ${TRIP_UPDATE_URL}?appID=${appID} \
